0dd5821648ff376e61ed041a343b49dbeeeb15f5
oza-pong <risei.heian.kashiba@gmail.com>
2017-01-20 14:23:37 +0900

    f

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -1,10 +1,5 @@
-var viewer = new Cesium.Viewer('cesiumContainer', {
-imageryProvider: new Cesium.OpenStreetMapImageryProvider({
-url: 'http://cyberjapandata.gsi.go.jp/xyz/std/',
-credit: new Cesium.Credit('地理院タイル', '', 'http://maps.gsi.go.jp/development/ichiran.html')
-}),
-baseLayerPicker: false
-});
+var viewer = new Cesium.Viewer('cesiumContainer');
+
 
 //kml読み込み
 viewer.dataSources.add(Cesium.KmlDataSource.load(''));
@@ -13,4 +8,3 @@ viewer.dataSources.add(Cesium.KmlDataSource.load(''));
 //視点の変更
 viewer.camera.flyTo({
     destination : Cesium.Cartesian3.fromDegrees(135.69, 34.69, 5000.0)});
-
